392a8ad61ea6bc30546b92907c67c7761e53e22a
Gia Huy <nguyengiahuy16@gmail.com>
2021-03-07 16:00:36 +0700

    add config nginx

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# nginx config
+path_nginx=/etc/nginx/sites-enabled/heo.giayuh.com
+if test -f $path_nginx; then
+  rm $path_nginx
+fi
+cp nginx.conf path_nginx
+systemctl restart nginx
+
+
 # image docker
 image=zayuh/heo
 
