69f196f20532595ef50e5d60254b40c638486524
Gia Huy <nguyengiahuy16@gmail.com>
2021-03-07 13:09:48 +0700

    add shell add user

---
+++ scripts/add-user.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+USR=$1
+if [[ -z $USR ]]; then
+  help
+  exit 1
+fi
+
+# add user
+useradd -m -d "/home/$USR" -s /bin/bash "$USR"
+usermod -aG docker "$USR"
+
+# setup ssh
+chmod 700 "/home/$USR/.ssh"
+touch "/home/$USR/.ssh/authorized_keys"
+chmod 600 "/home/$USR/.ssh/authorized_keys"
+
+# add folder data docker
+mkdir "/home/$USR/data"
+chown -R "$USR:users" "/home/$USR/data"
