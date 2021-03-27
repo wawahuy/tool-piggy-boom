81ad56869ead8e5977bdd050450ea6ebca307f85
Gia Huy <nguyengiahuy16@gmail.com>
2021-03-27 12:10:41 +0700

    test time

--- server/src/modules/admin/home/home.controller.ts
+++ server/src/modules/admin/home/home.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
+import moment from 'moment';
 
 export default class HomeController {
   static homeView(req: Request, res: Response) {
-    res.render("admin/home/home.view.ejs");
+    const data = {
+      timeUTC: moment().utc().format('YYYY/MM/DD HH:mm:ssZ'),
+    }
+    res.render("admin/home/home.view.ejs", data);
   }
 }
